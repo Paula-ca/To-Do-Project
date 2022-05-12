b44c0936bff1e3f51772fd3cee7315b3ea2a0ff7
Paula-ca <gonzalezpaula17@hotmail.com>
2022-05-12 20:16:35 -0300

    new project

---
+++ scripts/login.js
@@ -0,0 +1,56 @@
+
+window.addEventListener('load', function () {
+    /* ---------------------- obtenemos variables globales ---------------------- */
+   const formLogin = document.forms[0];
+   const inputEmail = document.getElementById("inputEmail")
+   const inputPass = document.getElementById("inputPassword")
+   const url = 'https://ctd-todo-api.herokuapp.com/v1/users/login';
+   
+    /* -------------------------------------------------------------------------- */
+    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+    /* -------------------------------------------------------------------------- */
+    formLogin.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        const usuario = {
+            email: inputEmail.value,
+            password: inputPass.value
+        };
+
+        const settings = {
+            method: 'POST',
+            body: JSON.stringify(usuario),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        
+        fetch(url, settings).then(response => {
+            return response.json()
+          }).then(respuesta => {
+            realizarLogin(respuesta)
+          })
+          .catch(error => {
+            console.log(`Ocurrió un error: ${error}`);
+          })
+    });
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                     FUNCIÓN 2: Realizar el login [POST]                    */
+    /* -------------------------------------------------------------------------- */
+    function realizarLogin(settings) {
+        if (settings.jwt) {
+            localStorage.setItem('jwt', JSON.stringify(settings.jwt));
+            location.replace('/mis-tareas.html');
+          } else {
+            const span = document.querySelector(".campo-login");
+            span.innerHTML= `${settings}`;
+            console.log(`Ocurrió un error: ${settings}`);
+          }
+      };
+    
+
+
+});
